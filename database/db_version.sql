6ca4e91c6f17db58ad58b540b56cedaeec75a721
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2020-08-18 19:46:29 -0300

    refactoring pra inglês

--- database/db_version.sql
+++ database/db_version.sql
@@ -1,7 +1,40 @@
 -- 16/07/2020
-CREATE TABLE IF NOT EXISTS imagens_produtos (
-    id_imagem INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    id_produto INT,
-    caminho VARCHAR(255),
-    FOREIGN KEY (id_produto) REFERENCES produtos (id_produto)
+CREATE TABLE IF NOT EXISTS productImages (
+    imageId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    productId INT,
+    path VARCHAR(255),
+    FOREIGN KEY (productId) REFERENCES products (productId)
+);
+
+-- refactoring pra inglês
+drop table pedidos;
+drop table usuarios;
+drop table imagens_produtos;
+drop table produtos;
+
+CREATE TABLE IF NOT EXISTS products (
+    productId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(45),
+    price FLOAT,
+    productImage VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    orderId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    productId INT,
+    quantity INT,
+    FOREIGN KEY (productId) REFERENCES products (productId)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    userId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(100),
+    password VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS productImages (
+    imageId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    productId INT,
+    path VARCHAR(255),
+    FOREIGN KEY (productId) REFERENCES products (productId)
 );
