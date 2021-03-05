c413341019fde778f8560151f920e5708c2db8c3
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-03-05 16:58:26 -0300

    feat(pix, gerencianet): gera QrCode com API Pix Gerencianet

--- controllers/order-controller.js
+++ controllers/order-controller.js
@@ -1,4 +1,8 @@
 const mysql = require('../mysql');
+const fs = require('fs');
+const https = require('https');
+const axios = require('axios');
+const randexp = require('randexp');
 
 exports.getOrders = async (req, res, next) => {
     try {
@@ -121,5 +125,125 @@ exports.deleteOrder = async (req, res, next) => {
     } catch (error) {
         return res.status(500).send({ error: error });
     }
-
 };
+
+exports.oAuthGerencianet = async (req, res, next) => {
+    try {
+        const cert = fs.readFileSync('prod282386.p12');
+        const credentials = process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET;
+        const auth = Buffer.from(credentials).toString('base64');
+        const agent = https.Agent({
+            pfx: cert,
+            passphrase: ''
+        });
+        const data = JSON.stringify({ "grant_type": "client_credentials" });
+
+        res.locals.agent = agent
+
+        const config = {
+            method: 'POST',
+            url: 'https://api-pix.gerencianet.com.br/oauth/token',
+            headers: {
+                Authorization: 'Basic ' + auth,
+                'Content-Type': 'application/json'
+            },
+            httpsAgent: agent,
+            data: data
+        }
+
+        axios(config)
+            .then(response => {
+                res.locals.accessToken = response.data.access_token;
+                console.log('Auth', response.data);
+                next();
+            })
+            .catch(erro => {
+                console.log(error);
+                return res.status(500).send({ error: error });
+            })
+
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
+}
+
+exports.createPixBilling = async (req, res, next) => {
+    try {
+        const data = JSON.stringify({
+            "calendario": { "expiracao": 3600 },
+            "devedor": {
+              "cpf": req.body.payee.cpf,
+              "nome": req.body.payee.name
+            },
+            "valor": { "original": req.body.value },
+            "chave": process.env.CHAVE_PIX,
+            "solicitacaoPagador": req.body.description
+          });
+
+        txId = new randexp(/^[a-zA-Z0-9]{26,35}$/).gen();
+
+        const config = {
+            method: 'PUT',
+            url: `https://api-pix.gerencianet.com.br/v2/cob/${txId}`,
+            headers: {
+                Authorization: 'Bearer ' + res.locals.accessToken,
+                'Content-Type': 'application/json'
+            },
+            httpsAgent: res.locals.agent,
+            data: data
+        }
+
+        axios(config)
+            .then(response => {
+                res.locals.billing = response.data;
+                console.log('Billing', response.data);
+                next();
+            })
+            .catch(erro => {
+                console.log(error);
+                return res.status(500).send({ error: error });
+            })
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
+}
+
+exports.getQrCode = async (req, res, next) => {
+    try {
+        const locId = res.locals.billing.loc.id;
+        const config = {
+            method: 'GET',
+            url: `https://api-pix.gerencianet.com.br/v2/loc/${locId}/qrcode`,
+            headers: {
+                Authorization: 'Bearer ' + res.locals.accessToken,
+                'Content-Type': 'application/json'
+            },
+            httpsAgent: res.locals.agent
+        }
+
+        axios(config)
+            .then(response => {
+                imgQrCode = decodeBase64Image(response.data.imagemQrcode);
+                fs.writeFileSync(`qrcodes/pix-billing-${locId}.jpg`, imgQrCode.data)
+                console.log('QrCode', response.data);
+                next();
+            })
+            .catch(erro => {
+                console.log(error);
+                return res.status(500).send({ error: error });
+            })
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
+}
+
+function decodeBase64Image(dataString) {
+    var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
+    if (matches.length !== 3) {
+        return new Error('Invalid input string');
+    }
+    response.type = matches[1];
+    response.data = new Buffer.from(matches[2], 'base64');
+    return response;
+}
--- routes/order-route.js
+++ routes/order-route.js
@@ -3,9 +3,13 @@ const router = express.Router();
 
 const orderController = require('../controllers/order-controller');
 
-router.get('/', orderController.getOrders);
-router.post('/', orderController.postOrder);
-router.get('/:orderId', orderController.getOrderDetail);
-router.delete('/:orderId', orderController.deleteOrder);
+router.get('/',                         orderController.getOrders);
+router.post('/',                        orderController.postOrder);
+router.get('/:orderId',                 orderController.getOrderDetail);
+router.delete('/:orderId',              orderController.deleteOrder);
+
+router.post('/:orderId/pix/billing',    orderController.oAuthGerencianet,
+                                        orderController.createPixBilling,
+                                        orderController.getQrCode)
 
 module.exports = router;
