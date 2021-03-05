4e0d9caabe4533802f5b7a21b23ce7890604e535
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-03-05 14:26:58 -0300

    Revert "feat(gerencianet, api, pix): gera cobrança e qrCode"
    This reverts commit b83f8f03f819415053397767244c33f50eb5c14e.

--- routes/order-route.js
+++ routes/order-route.js
@@ -3,14 +3,9 @@ const router = express.Router();
 
 const orderController = require('../controllers/order-controller');
 
-router.get('/',                         orderController.getOrders);
-router.post('/',                        orderController.postOrder);
-router.get('/:orderId',                 orderController.getOrderDetail);
-router.delete('/:orderId',              orderController.deleteOrder);
-
-router.post('/:orderId/pix/billing',    orderController.oAuthGerencianet,
-                                        orderController.createPixBilling,
-                                        orderController.getQrCode,
-                                        orderController.savePixBilling);
+router.get('/', orderController.getOrders);
+router.post('/', orderController.postOrder);
+router.get('/:orderId', orderController.getOrderDetail);
+router.delete('/:orderId', orderController.deleteOrder);
 
 module.exports = router;
